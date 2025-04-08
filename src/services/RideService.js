1a3cd2ce8d386f8d5317a1eb64397c2d6823e138
Valery Leontyev <feedbee@gmail.com>
2025-04-08 21:53:37 +0000

    Fix bot fainling on commands with reply in topics

--- src/services/RideService.js
+++ src/services/RideService.js
@@ -380,7 +380,7 @@ export class RideService {
     }
     
     // Then check replied message
-    if (message.reply_to_message) {
+    if (message.reply_to_message?.text) {
       const originalMessage = message.reply_to_message.text;
       const rideIdMatch = originalMessage.match(/🎫\s*#Ride\s*#(\w+)/i);
       
