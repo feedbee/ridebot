8be04d494ff7af6d38a32b73861d7b6db1418e55
Valery Leontyev <feedbee@gmail.com>
2025-10-03 17:29:19 +0000

    Refactoring: use the existing formatUpdateResultMessage() method consistently in the UpdateRideCommandHandler

--- src/commands/UpdateRideCommandHandler.js
+++ src/commands/UpdateRideCommandHandler.js
@@ -81,15 +81,7 @@ export class UpdateRideCommandHandler extends BaseCommandHandler {
     const result = await this.updateRideMessage(updatedRide, ctx);
     
     if (result.success) {
-      let reply = '';
-      if (result.updatedCount > 0) {
-        reply = `Ride updated successfully. Updated ${result.updatedCount} message(s).`;
-      } else {
-        reply = `Ride has been updated, but no messages were updated. You may want to /postride the ride in the chats of your choice again, they could have been removed.`;
-      }
-      if (result.removedCount > 0) {
-        reply += ` Removed ${result.removedCount} unavailable message(s).`;
-      }
+      const reply = this.formatUpdateResultMessage(result, 'updated');
       await ctx.reply(reply);
     } else {
       await ctx.reply(`Ride has been updated, but there was an error updating the ride message. You may need to create a new ride message.`);
